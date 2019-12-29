3ab4169f1b403ad6de69f79b30b8674b94b0d6a1
ecembalkaya <ecm.ecm23@gmail.com>
2019-12-29 07:50:18 -0500

    README.MD updated, comments added

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 Project repository for SE362 Course. 
+
+Purpose of this project is learning git operations, clone, push, pull request etc.
+
+Students:
+Batuhan Baran
+Ecem Balkaya
+Eylul Ucan
+Gokhan Gonder
+
+usage of this simple currency converter:
+
+run on your terminal with:
+
+python3 curr_converter.py
+
+then enter currencies with their codes, like "USD"
+then enter amounts
+This simple program uses fixer.io API
+
+
--- curr_converter.py
+++ curr_converter.py
@@ -1,31 +1,41 @@
+# Python program to convert the currency 
+# of one country to that of another country 
+
+# Import the modules needed 
 import requests 
 import datetime
 
 class Currency_convertor: 
+	# empty dict to store the conversion rates 
 	rates = {} 
 	def __init__(self, url): 
 		data = requests.get(url).json() 
 
+		# Extracting only the rates from the json data 
 		self.rates = data["rates"] 
 
+	# function to do a simple cross multiplication between 
+	# the amount and the conversion rates 
 	def convert(self, from_currency, to_currency, amount): 
 		initial_amount = amount 
 		if from_currency != 'EUR' : 
 			amount = amount / self.rates[from_currency] 
 
+		# limiting the precision to 2 decimal places 
 		amount = round(amount * self.rates[to_currency], 2) 
 		print('{} {} = {} {}'.format(initial_amount, from_currency, amount, to_currency)) 
 
+# Driver code 
+if __name__ == "__main__": 
 
-f __name__ == "__main__": 
-
-	now = datetime.datetime.now()
 	YOUR_ACCESS_KEY = '62212464071d545dd78f45b57721429c' 
 	url = str.__add__('http://data.fixer.io/api/latest?access_key=', YOUR_ACCESS_KEY) 
 	c = Currency_convertor(url) 
 	from_country = input("From Country: ") 
 	to_country = input("TO Country: ") 
 	amount = int(input("Amount: ")) 
+
+	now = datetime.datetime.now()
 
 
 	c.convert(from_country, to_country, amount) 
